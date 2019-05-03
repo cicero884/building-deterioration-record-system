10be61005b4d1868125773d81d959f18b94d325a
cicero884 <cicero884@gmail.com>
2019-05-03 10:36:14 +0800

    Include js into upperBar

--- view/utils/upperBar.php
+++ view/utils/upperBar.php
@@ -4,4 +4,5 @@
 		<?php echo $_SESSION['userId'];?>
 	</h1>
 	<img id='hamburger' src='../image/buttons/hamburger.png'></img>
+	<script type="text/javascript" src="upperBar.js" defer="defer"></script>
 </div>
