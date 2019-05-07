9995da7ddc574d4fb8a65c997415ea2ebfb4fad4
cicero884 <cicero884@gmail.com>
2019-05-07 17:05:42 +0800

    Put utility back

---
+++ view/app/main/main.php
@@ -0,0 +1,11 @@
+<div>
+<h2 id="subTitle">Recently</h2>
+<div id="home_list">
+<?php
+	echo $recentHouseHTML;
+?>
+</div>
+<button id="add_house">
+	<img src="../image/buttons/new.png">
+</button>
+</div>
