652889a8927dd883c8154435ef61c6a4f35cb367
林謙慈 <amylin19990210@gmail.com>
2019-05-12 11:33:57 +0800

    add model deterioration

--- models/floor.php
+++ models/floor.php
@@ -2,12 +2,12 @@
 require_once("config.php");
 
 class ModelFloor {
-        // insert building information to the database
-        public function insertFloorInfo( $floorInfo ) {
-            $sql = "INSERT INTO floor ( buildingId, floor, floorPlan )
-                    VALUES ( :buildingId, :floor, :floorPlan ); ";
-            $insert = $GLOBALS['conn']->prepare( $sql ); 
-            $insert->execute( $floorInfo );
-        }
+    // insert building information to the database
+    public function insertFloorInfo( $floorInfo ) {
+        $sql = "INSERT INTO floor ( buildingId, floor, floorPlan )
+                VALUES ( :buildingId, :floor, :floorPlan ); ";
+        $insert = $GLOBALS['conn']->prepare( $sql ); 
+        $insert->execute( $floorInfo );
+    }
 }
 ?>
