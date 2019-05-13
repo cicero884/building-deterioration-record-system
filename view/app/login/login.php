da85312e84774978dc0a3ce103413479290d0954
林謙慈 <amylin19990210@gmail.com>
2019-05-14 00:07:54 +0800

    完成一半的 login css

--- view/app/login/login.php
+++ view/app/login/login.php
@@ -1,14 +1,26 @@
-<img/>
-<form id="app__login" method="post">
-    <section>
-        <img/>
-        <input type="text" name="account" id="login__account">
+<header class="header">
+    <img class="header__logo" src="view/web/image/logo.png" >
+    <h1  class="header__title"> Save every detail. Check building safety.</h1> 
+    <section class="header__link">
+        <a id="link__login" class="link__item" href=" ">會員登入</a>
+        <a id="link__about" class="link__item" href=" ">關於我們</a>
+        <a id="link__design" class="link__item" href=" ">設計理念</a>
     </section>
-    <section>
-        <img/>
-        <input type="password" name="password" id="login__password">
-    </section>
-    <button type="button" id="login__button">Login</button>
-	<p id="errMsg"></p>
-</form>
-<button type="button">立即註冊</button>
+</header>
+
+<main class="content">
+    <img class="content__logo" src="view/app/image/logos/login.png"/>
+    <form id="app__login" method="post">
+        <section class="login__session">
+            <img class="session__logo" src="view/app/image/icons/account.png"/>
+            <input class="session__input" type="text" name="account" id="login__account">
+        </section>
+        <section class="login__session">
+            <img class="session__logo" src="view/app/image/icons/password.png"/>
+            <input type="password" name="password" id="login__password">
+        </section>
+        <button type="button" id="login__button">Login</button>
+        <p id="errMsg"></p>
+    </form>
+    <button type="button">立即註冊</button>
+</main>
