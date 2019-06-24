b7c9fc555b6c4f6b70042476947b0d798d0be5a4
林謙慈 <amylin19990210@gmail.com>
2019-06-24 22:33:04 +0800

    fix app/house image upload

---
+++ view/app/deterioration/deterioration.js
@@ -0,0 +1,23 @@
+$('#final').click(function(e){
+    e.preventDefault();
+    let hashData=window.location.hash.substring(1).split('-');
+	let formData = new FormData($('.content')[0]);
+	formData.append('page', 'deterioration');
+    formData.append('action','update');
+    formData.append('deterioration', hashData[3]);
+    console.log( hashData );
+	formData.set('houseImage',$('[name="houseImage"]').prop('files')[0]);
+	$.ajax({
+        url:'upload.php',
+        type:'POST',
+		processData: false,
+		contentType: false,
+        data:formData,
+        error: function(xhr) {
+            alert('Ajax request error');
+        },
+        success: function(response){
+			window.location.hash="app_main";
+        }
+    });
+})
--- view/app/house/house.js
+++ view/app/house/house.js
@@ -18,3 +18,19 @@ $('#submitHouse').click(function(e){
         }
     });
 })
+
+document.getElementById("houseImage").onchange = () => {
+    readURL( document.getElementById("houseImage") );
+}
+
+let readURL = (input)=> {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementsByClassName("preview")[0].('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
