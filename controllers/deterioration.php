2fe4e2162c77707332725ad1046a6fe961a54e5e
林謙慈 <amylin19990210@gmail.com>
2019-06-25 04:59:10 +0800

    I find the bug

--- controllers/deterioration.php
+++ controllers/deterioration.php
@@ -16,6 +16,7 @@ class DeteriorationController {
 
     public function updateData(){
 
+        $deteriorationId = $_POST['deteriorationId'];
         $imageUpload = array();
         for($n = 1; $n <= 4; $n += 1) {
             array_push( $imageUpload, $this->controllers['image']->imageUpload( "image".$n, "deterioration", $_POST['buildingId'], $_POST['floorId'], $deteriorationId, $n ) );
